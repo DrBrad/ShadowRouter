3178ce037f3343c2e33d17b9c77087ce346c027a
DrBrad <brad@bradeagle.com>
2024-03-26 13:10:13 -0600

    Kademlia Base change

--- src/unet/shadowrouter/ShadowRouter.java
+++ src/unet/shadowrouter/ShadowRouter.java
@@ -1,7 +1,13 @@
 package unet.shadowrouter;
 
 import unet.kad4.Kademlia;
+import unet.kad4.kad.KademliaBase;
+import unet.kad4.messages.*;
+import unet.kad4.refresh.tasks.BucketRefreshTask;
+import unet.kad4.refresh.tasks.StaleRefreshTask;
 import unet.kad4.routing.inter.RoutingTable;
+import unet.kad4.rpc.JoinNodeListener;
+import unet.kad4.rpc.KRequestListener;
 import unet.kad4.utils.Node;
 import unet.shadowrouter.kad.SRequestListener;
 import unet.shadowrouter.tunnel.tcp.RelayServer;
@@ -9,9 +15,52 @@ import unet.shadowrouter.tunnel.tcp.RelayServer;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.util.List;
 
-public class ShadowRouter implements RoutingTable.RestartListener {
+public class ShadowRouter extends KademliaBase {
+
+    public ShadowRouter(RoutingTable routingTable){
+        super(routingTable);
+        BucketRefreshTask bucketRefreshTask = new BucketRefreshTask();
+
+        routingTable.addRestartListener(new RoutingTable.RestartListener(){
+            @Override
+            public void onRestart(){
+                bucketRefreshTask.execute();
+            }
+        });
+
+        try{
+            registerRequestListener(new KRequestListener());
+
+            registerMessage(PingRequest.class);
+            registerMessage(PingResponse.class);
+            registerMessage(FindNodeRequest.class);
+            registerMessage(FindNodeResponse.class);
+            registerMessage(GetPortRequest.class);
+            registerMessage(GetPortResponse.class);
+
+            refresh.addOperation(bucketRefreshTask);
+            refresh.addOperation(new StaleRefreshTask());
+
+        }catch(NoSuchFieldException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void join(int localPort, InetSocketAddress address)throws IOException {
+        super.join(localPort, address);
+
+        FindNodeRequest request = new FindNodeRequest();
+        request.setDestination(address);
+        request.setTarget(routingTable.getDerivedUID());
+        server.send(request, new JoinNodeListener(this));
+    }
+
+
+
+
 
     private Kademlia kad;
     private RelayServer relay;
